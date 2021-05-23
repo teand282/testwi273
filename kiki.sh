7d2cc3105b005c02a79110b089ae5d3ef6edc26d
awdn2929 <Hshu378@outlook.com>
2021-05-23 14:04:11 +0530

    commit messadjs

--- kiki.sh
+++ kiki.sh
@@ -4,37 +4,22 @@ u="c"
 while [ $u = "c" ]
 do
    touch teemy.txt
-   sleep 10
+   sleep 5
    git add .
-   sleep 10
+   sleep 5
    git commit -m "commit messadjs" 
-   sleep 10
+   sleep 5
    git push origin main --force
-   sleep 30
+   sleep 15
    rm teemy.txt
-   sleep 10
+   sleep 5
    git add .
-   sleep 10
+   sleep 5
    git commit -m "commit messadjs" 
-   sleep 10
+   sleep 5
    git push origin main --force   
-   sleep 30
-   touch teemy.txt
-   sleep 10
-   git add .
-   sleep 10
-   git commit -m "commit messadjs" 
-   sleep 10
-   git push origin main --force
-   sleep 30
-   rm teemy.txt
-   sleep 10
-   git add .
-   sleep 10
-   git commit -m "commit messadjs" 
-   sleep 10
-   git push origin main --force   
-   sleep 300
+   sleep 15
+   
    
 done
 
